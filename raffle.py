3b388375d02c5f89f9aac9f3a8865817ab7bf12e
hagutis <53209183+hagutis@users.noreply.github.com>
2022-12-01 01:44:44 -0800

    Update raffle.py

--- raffle.py
+++ raffle.py
@@ -17,42 +17,43 @@ st.title('🎰 RAFFLE 🧧')
 
 add_selectbox = st.sidebar.text('Powered by: MPoint Analytics LLC.\n© Copyright 2022')
 
-def cloud(text):
+def cloud(kids):
     
-    wc = WordCloud(background_color="white", colormap="hot",
-    max_font_size=300, random_state=42)
+    if st.button("START"):
+        while len(kids)>1:
+            random.shuffle(kids)
+            text = ','.join(kids)
+            kids.pop()
+    
+            wc = WordCloud(background_color="white", colormap="hot",
+            max_font_size=300, random_state=42)
 
-    # generate word cloud
-    wc.generate(text)
+            # generate word cloud
+            wc.generate(text)
 
-    # create coloring from image
-    #image_colors = ImageColorGenerator(image)
+            # create coloring from image
+            #image_colors = ImageColorGenerator(image)
 
-    # show the figure
-    plt.figure(figsize=(50,50))
-    fig, axes = plt.subplots(1,2, gridspec_kw={'width_ratios': [5, 4]})
-    axes[0].imshow(wc, interpolation="bilinear")
-    # recolor wordcloud and show
-    # we could also give color_func=image_colors directly in the constructor
-    #axes[1].imshow(image, cmap=plt.cm.gray, interpolation="bilinear")
+            # show the figure
+            plt.figure(figsize=(50,50))
+            fig, axes = plt.subplots(1,2, gridspec_kw={'width_ratios': [5, 4]})
+            axes[0].imshow(wc, interpolation="bilinear")
+            # recolor wordcloud and show
+            # we could also give color_func=image_colors directly in the constructor
+            #axes[1].imshow(image, cmap=plt.cm.gray, interpolation="bilinear")
 
-    for ax in axes:
-        ax.set_axis_off()
-    placeholder=st.empty()
-    placeholder.pyplot(fig, clear_figure=True)
-    time.sleep(0.5)
-    placeholder.empty()
+            for ax in axes:
+                ax.set_axis_off()
+            placeholder=st.empty()
+            placeholder.pyplot(fig, clear_figure=True)
+            time.sleep(0.5)
+            placeholder.empty()
 
 
 def main():
     st.write("# Text Summarization with a WordCloud")
     kids=['Christoff','Cedrick','Chaskell','Jasmine','Nathan','Arianne','Rye','TJ','Annika','Amielle','Aaron','Aedan','Angel','Niall','Ardene','Jasen','Shiloh','Abbey','Ava','Rayden','Edmund','Matt','Luigi','Abby']
-    if st.button("START"):
-        while len(kids)>1:
-            random.shuffle(kids)
-            text = ','.join(kids)
-            st.write(cloud(text), use_column_width=True)
-            kids.pop()
+    cloud(kids)
                 
                 
 
