000b05f17d31b7a72b0717fd2873f50c1512ffe2
hagutis <53209183+hagutis@users.noreply.github.com>
2022-12-02 22:15:07 -0800

    Update raffle.py

--- raffle.py
+++ raffle.py
@@ -37,6 +37,8 @@ def cloud(kids):
         ax.set_axis_off()
     placeholder=st.empty()
     placeholder.pyplot(fig)
+    placeholder.empty()
+    
     if st.button("START"):
         while len(kids)>0:
             random.shuffle(kids)
