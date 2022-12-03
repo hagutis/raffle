b4ef54c56bd4861cdc22267c54486b9319272159
hagutis <53209183+hagutis@users.noreply.github.com>
2022-12-02 22:53:28 -0800

    Update raffle.py

--- raffle.py
+++ raffle.py
@@ -72,7 +72,9 @@ def cloud(kids):
             winner=temp_kids[0]
             temp_kids.pop()
         placeholder.pyplot(fig)
+        st.write(winner)
         kids.remove(winner)
+        st.write(kids)
         
 
 
