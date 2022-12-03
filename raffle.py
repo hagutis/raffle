53c1c2b0083af40688c806d0a07838c5125ccd4e
hagutis <53209183+hagutis@users.noreply.github.com>
2022-12-02 23:43:00 -0800

    Update raffle.py

--- raffle.py
+++ raffle.py
@@ -18,7 +18,7 @@ def cloud(kids):
     random.shuffle(temp_kids)
     text = ','.join(temp_kids)
     colormap='magma'
-    colormap='binary'
+    colormap='rainbow'
     wc = WordCloud(background_color="white", colormap=colormap,
     max_font_size=300, random_state=42)
 
