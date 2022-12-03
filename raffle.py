0e656367dc369e176179529dad91308fd1095672
hagutis <53209183+hagutis@users.noreply.github.com>
2022-12-02 22:13:04 -0800

    Update raffle.py

--- raffle.py
+++ raffle.py
@@ -9,20 +9,38 @@ import random
 import time
 
 
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
-
 st.title('🎰 RAFFLE 🧧')
 
 add_selectbox = st.sidebar.text('Powered by: MPoint Analytics LLC.\n© Copyright 2022')
 
 def cloud(kids):
+    random.shuffle(kids)
+    text = ','.join(kids)
+    wc = WordCloud(background_color="white", colormap="magma",
+    max_font_size=300, random_state=42)
+
+    # generate word cloud
+    wc.generate(text)
+
+    # create coloring from image
+    #image_colors = ImageColorGenerator(image)
+
+    # show the figure
+    plt.figure(figsize=(50,50))
+    fig, axes = plt.subplots(1,2, gridspec_kw={'width_ratios': [5, 4]})
+    axes[0].imshow(wc, interpolation="bilinear")
+    # recolor wordcloud and show
+    # we could also give color_func=image_colors directly in the constructor
+    #axes[1].imshow(image, cmap=plt.cm.gray, interpolation="bilinear")
+
+    for ax in axes:
+        ax.set_axis_off()
+    placeholder=st.empty()
+    placeholder.pyplot(fig)
     if st.button("START"):
         while len(kids)>0:
             random.shuffle(kids)
             text = ','.join(kids)
-            placeholder=st.empty()
             wc = WordCloud(background_color="white", colormap="magma",
             max_font_size=300, random_state=42)
 
@@ -42,7 +60,7 @@ def cloud(kids):
 
             for ax in axes:
                 ax.set_axis_off()
-            
+            placeholder=st.empty()
             placeholder.pyplot(fig)
             time.sleep(1)
             placeholder.empty()
